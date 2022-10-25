36da1ddf9d17b6d71131589e31ff5260ff1311ec
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-25 12:49:04 +0200

    Edit index.js

---
+++ src/index.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", () => {
+	//Mind
+	document.getElementById("mind").addEventListener("click", async () => {
+		let response = await fetch("products.json");
+		let result = await response.json();
+		printData(result.products);
+	});
+
+	//ABC order
+	document.getElementById("abc").addEventListener("click", async () => {
+		let response = await fetch("products.json");
+		let result = await response.json();
+
+		let order = result.products.sort(function (a, b) {
+			let first = a.title.toUpperCase();
+			let second = b.title.toUpperCase();
+
+			if (first < second) {
+				return -1;
+			} else if (first > second) {
+				return 1;
+			} else {
+				return 0;
+			}
+		});
+		printData(order);
+	});
+
+	//Legdrágább
+	document.getElementById("legdragabb").addEventListener("click", async () => {
+		let response = await fetch("products.json");
+		let result = await response.json();
+
+		let expensive = result.products.sort((a, b) => {
+			if (a.price < b.price) {
+				return 1;
+			} else if (a.price > b.price) {
+				return -1;
+			} else {
+				return 0;
+			}
+		});
+		printData(expensive);
+	});
+
+	//Leírás
+	document.getElementById("leiras").addEventListener("click", async () => {
+		let response = await fetch("products.json");
+		let result = await response.json();
+		let filterezes = result.filter(() => {
+			return result.description.contains(
+				document.getElementById("szovegmezo").value
+			);
+		});
+		printData(filterezes);
+	});
+
+	//Ajánlat
+	document.getElementById("ajanlat").addEventListener("click", async () => {
+		let response = await fetch("products.json");
+		let result = await response.json();
+
+		//printData(termekLista);
+	});
+
+	function printData(termekek) {
+		let termekLista = document.getElementById("adatok");
+		termekLista.textContent = "";
+
+		for (let p of termekek) {
+			let li = document.createElement("li");
+			li.innerHTML =
+				p.id +
+				". " +
+				p.title +
+				" - " +
+				p.description +
+				"<br>Price: " +
+				p.price +
+				" - Discount: " +
+				p.discountPercentage +
+				"%<br>Rating: " +
+				p.rating +
+				" - Stock: " +
+				p.stock +
+				"<br>Brand: " +
+				p.brand +
+				" - Category: " +
+				p.category +
+				"<hr>";
+			termekLista.appendChild(li);
+		}
+	}
+});
+
+//let appleTermekek = result.products.filter(e => e.brand === 'Apple')
